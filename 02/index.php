1a35aaafc6b324059a6ffb2e0b3e007758b4a4fe
nansz <nansz@mail.ru>
2018-03-05 00:12:58 +0200

    Сlasswork 2

---
+++ 02/index.php
@@ -0,0 +1,38 @@
+
+<?php
+$loc = false;
+echo $loc."</p>";
+echo !$loc. "</p>";
+echo $loc."</p>";
+$number = 7;
+echo ($number % 2)."</p>";
+
+$a=4; $b=6;
+echo $a." ".$b."</p>";
+
+$c=$a;
+$a=$b;
+$b=$c;
+
+echo $a." ".$b."</p>";
+
+$a =$a ^ $b;
+echo $a."</p>";
+$b =$b ^ $a;
+echo $b."</p>";
+$a =$a ^ $b;
+echo $a." ".$b."</p>";
+
+if ($a>$b)
+   {
+       echo "a> b"."</p>";
+   }
+  else
+  {
+      echo "a < b"."</p>";
+  }
+
+  var_dump()
+      ?>
+
+
