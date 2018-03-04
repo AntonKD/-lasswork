54b841d756a685b9d05b607368ff55071429bb53
nansz <nansz@mail.ru>
2018-03-05 00:13:48 +0200

    Сlasswork 5

---
+++ 05/index.php
@@ -0,0 +1,14 @@
+<?php
+
+        function print_full_name(string $first_name, string $last_name) : void
+
+    {
+
+            echo $first_name." ".$last_name;
+
+
+    }
+$first_name ='iva';
+ $last_name ='ivanov';
+print_full_name($first_nameiva, $last_nameIvan);
+
